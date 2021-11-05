9a6bc9a2718f3d132cd08c04fd03ef56f76a0459
Carlos <carlos.cast.mar@gmail.com>
2021-11-05 18:39:25 +0100

    format modified and summary added

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ PygmentsBridge.latex_formatter = CustomLatexFormatter
 # -- Project information -----------------------------------------------------
 master_doc = 'index'
 
-project = 'TFG microprocessor benchmark'
+project = 'Estudio del comportamiento de micro-ROS. Un análisis teórico y práctico.'
 copyright = '2021, Carlos Castillo Martínez'
 author = 'Carlos Castillo Martínez'
 
@@ -85,13 +85,13 @@ latex_custom = r'''
 
    \fancypagestyle{normal}{
     \fancyhf{}
-    \fancyfoot[RO]{{\py@HeaderFamily ETS Ingenieros Industriales \thepage}}
-    \fancyfoot[LO]{{\py@HeaderFamily\nouppercase{\rightmark}}}
+    \fancyfoot[RO]{{\py@HeaderFamily \thepage}}
+    \fancyfoot[LO]{{\py@HeaderFamily Carlos Castillo Martínez}}
     \fancyhead[RO]{{\py@HeaderFamily \@title\sphinxheadercomma\py@release}}
     \if@twoside
      \fancyfoot[LE]{{\py@HeaderFamily\thepage}}
-     \fancyfoot[RE]{{\py@HeaderFamily\nouppercase{\leftmark}}}
-     \fancyhead[LE]{{\py@HeaderFamily \@title\sphinxheadercomma\py@release}}
+     \fancyfoot[RE]{{\py@HeaderFamily Escuela Técnica Superior de Ingenieros Industriales (UPM)}}
+     \fancyhead[LE]{{\py@HeaderFamily \nouppercase{\leftmark}}}
     \fi
     \renewcommand{\headrulewidth}{0.4pt}
     \renewcommand{\footrulewidth}{0.4pt}
@@ -131,11 +131,13 @@ latex_maketitle = r'''
 \section*{Agradecimientos} % Se añade un asterisco a \section para que el título no esté numerado.
 %\addcontentsline{toc}{chapter}{Agradecimientos} % Al utilizar \section* se ha de añadir manualmente el apartado al índice (Table Of Contents, TOC).
 
-Agradezco a \dots
+En primer lugar quería agradecerle a mi familia el haber sido un apoyo incondicional para mi en todo momento, por celebrar mis éxitos como si de los suyos se tratara y por evitar que me hundiese en los momentos más difíciles.
 
-Gracias a \dots
+Le doy las gracias a mi tutor, Ricardo Sanz, por haberme brindado la oportunidad de realizar este proyecto y aprender de él. También quería agradecérselo a la doctoranda Esther Aguado, por su disponibilidad y ayuda durante todo el trabajo, fundamentales para el desarrollo del mismo. 
 
-A \dots \ por \dots
+Por último, quería recordar a todos mis amigos y compañeros que me han acompañado en este largo viaje los últimos años. Sin ellos hubiera sido imposible llegar hasta este punto.
+
+Quiero dedicarles este trabajo especialmente a mis padres, por ser mis principales referentes en todo aquello que quiero llegar a ser en mi vida, tanto personal como profesionalmente.
 
 \afterpage{\blankpage}
 
@@ -145,7 +147,35 @@ A \dots \ por \dots
 
 \addcontentsline{toc}{chapter}{Resumen Ejecutivo} % Al utilizar \section* se ha de añadir manualmente el apartado al índice (Table Of Contents, TOC).
 
-Este documento constituye una guía (que sirve a su vez de plantilla) para la elaboración de informes de TFG o TFM
+Durante los últimos años, la tecnología ha ido creciendo a una velocidad exponencial en relación a etapas anteriores. Este hecho se traduce en la irrupción de la ya actual cuarta revolución industrial, también denominada industria 4.0.
+
+Este fenómeno viene marcado por la aparición de nuevas tecnologías que están relacionadas con el tratamiento masivo de datos o “big data” y el internet de las cosas (IoT).
+
+Este último concepto se refiere a una interconexión digital de objetos cotidianos con internet, lo cual genera un ecosistema de dispositivos inteligentes habilitados para recoger, enviar y actuar sobre los datos que adquieren de sus entornos.
+
+En este sentido, la robótica constituye un sector fundamental en esta novedosa tecnología, que aprovecha y potencia las posibilidades que los robots ofrecen. Así mismo, los sistemas embebidos juegan un papel clave en la creación de estos ecosistemas, ya que permiten implementarse de manera sencilla en una gran cantidad de objetos cotidianos a un reducido coste, manteniendo unas prestaciones más que suficientes para las tareas que van a acometer.
+
+Esto ha producido que el mundo de la robótica acelere su desarrollo a pasos agigantados en la última década, tanto a nivel de hardware como de software.
+
+ROS es una tecnología pionera en el control de robots en tiempo real. Su crecimiento durante estos años está permitiendo a día de hoy la monitorización de muchos robots de una manera muy efectiva. Actualmente la versión en uso es ROS2. Sin embargo, recientemente se ha creado micro-ROS, una tecnología que acerca el mundo de ROS a los microcontroladores.
+
+En este trabajo se va a estudiar el comportamiento de esta tecnología. En una primera etapa se analizará la base del funcionamiento de las comunicaciones en tiempo real. En este apartado se incluirán las restricciones que dichos sistemas requieren y las ventajas que pueden aportar.
+
+Seguidamente se realizará un estudio teórico del funcionamiento de ROS. Esto incluye la base de programación que soporta ROS2, las distintas partes que forman la arquitectura del software y las herramientas que se utilizan para lograr integrar las funciones de ROS2 en microcontroladores.
+
+Finalmente se plantearán una serie de análisis que simulen aplicaciones que se puedan dar en la vida real y se medirá el comportamiento que presenta micro-ROS instalado en una placa ESP-32.
+
+Todo esto se realizará de manera autónoma sin ninguna experiencia previa en el sector, por lo que el trabajo resultante servirá también como guía de iniciación para la introducción a la programación del sistema operativo de robots.\\
+
+Codigos unesco:
+
+120317 Informática
+
+120323 Lenguajes de Programación
+
+120324 Teoría de la Programación
+
+120326 Simulación
 
 '''
 latex_elements = {
